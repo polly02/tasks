a25cd296bf81a7fd05746c6a452e4745160efd7b
polina <polina.zdanovich.02@mail.ru>
2023-01-18 16:31:19 +0300

    task

--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -256,6 +256,8 @@
 // вводит значение id: например, 1. Необходимо отобразить в консоль найденный
 // объект используя алгоритм бинарного поиска.
 
+//                ОШИБКА 
+
 // interface iProduct {
 //     id: number;
 //     title: string;
@@ -272,9 +274,34 @@
 //         { id: 5, title: 'Планшет', count: 22, price: 2100 },
 //     ]
 
-// function getObject(array: iProduct[]): object {
+// const btn: Element | null = document.querySelector(".btn")
+// const value: any | null = document.querySelector(".input")
 
+// btn?.addEventListener("click", () => {
+//     searchBinary(array)
+// })
+
+// function searchBinary(array: iProduct[]): object {
+//     let left = 1
+//     let right = array.length
+//     let middle = Math.floor((left + right) / 2)
+//     for (let i = 0; i < array.length; i++) {
+//         if (middle === value.value) {
+//             alert(`Данные по id: ${array[middle]}`)
+//             return value.value
+//         } else {
+//             if (middle < value.value) {
+//                 left = middle + 1
+//             } else {
+//                 right = middle - 1
+//             }
+//             middle = Math.floor((left + right) / 2)
+//         }
+//     }
+//     return array[middle]
 // }
+
+
 
 
 
@@ -298,4 +325,137 @@
 // }
 
 // console.log(countDublicate(array));
+
+// 12. Напишите программу для вывода чисел Фибоначчи используя рекурсию
+
+// без рекурсии
+
+// function fibonachi(num: number): number[] | number {
+//     let arr: number[] = [0, 1]
+//     if (num < 2) {
+//         return arr[num - 1]
+//     }
+//     for (let i = 2; i <= num; i++) {
+//         arr.push(arr[i - 1] + arr[i - 2])
+//     }
+//     return arr
+// }
+
+// console.log(fibonachi(5));
+
+// function fibonachi(num: number): number {
+//     if (num < 2) return num
+//     return fibonachi(num - 2) + fibonachi(num - 1)
+// }
+// console.log(fibonachi(4));
+
+// 13. Напишите программу для сортировки массива объектов из приложения по id,
+// использующую метод пузырька.
+
+// interface iProduct {
+//     id: number;
+//     title: string;
+//     count: number;
+//     price: number;
+// }
+
+// const arr: iProduct[] = [
+//     { id: 2, title: 'Смартфон', count: 33, price: 1500 },
+//     { id: 1, title: 'Часы', count: 10, price: 500 },
+//     { id: 4, title: 'Ноутбук', count: 13, price: 3000 },
+//     { id: 3, title: 'Моноблок', count: 6, price: 2200 },
+//     { id: 5, title: 'Планшет', count: 22, price: 2100 },
+// ]
+
+// function sortBubble(array: iProduct[]): iProduct[] {
+//     for (let j = array.length - 1; j > 0; j--) {
+//         for (let i = 0; i < j; i++) {
+//             if (array[i].id > array[i + 1].id) {
+//                 let middle = array[i]
+//                 array[i] = array[i + 1]
+//                 array[i + 1] = middle
+//             }
+//         }
+//     }
+//     return array
+// }
+
+// console.log(sortBubble(arr));
+
+// 14. Реализовать функцию f: при вызове f(2, 3) -> вернет 5, при вызове f(2)(3), тоже
+// вернет 5. Использовать каррирование
+
+// ???????????????????????
+// function summa(a: number): number {
+//     return (b: number) => {
+//         return a + b
+//     }
+// }
+// console.log(summa(2)(3));
+
+// let sum = (x: number) => (y: number) => x + y;
+// console.log(sum(2)(3));
+
+// 15. Более сложный вариант. Реализовать функцию f: при вызове f(1, 2, 3) -> вернет 6,
+// при вызове f(1)(2)(3)(), тоже вернет 6. Использовать каррирование
+
+// let sum = (x: number) => (y: number) => (z: number) => x + y + z
+// console.log(sum(1)(2)(3));
+
+// let multiply = (x: number) => (y: number) => (z: number) => x * y * z
+// console.log(multiply(1)(2)(3));
+
+// 16. Напишите код, который сделает из массива объект
+// [ {name: 'width', value: 10}, {name: 'height', value: 20} ] -> {width: 10, height: 20}
+
+// interface iInformation {
+//     name: string;
+//     value: number;
+// }
+
+// const arr: iInformation[] = [
+//     { name: 'width', value: 10 },
+//     { name: 'height', value: 20 }
+// ]
+
+// function transform(arr: iInformation[]): object {
+//     let res = {} as any
+//     for (let i = 0; i < arr.length; i++) {
+//         let { name, value } = arr[i]
+//         res[name] = value
+//     }
+//     return res
+// }
+
+// console.log(transform(arr));
+
+
+// 18. Дан массив с объектами даты. Необходимо отсортировать по датам.
+
+interface iDate {
+    date: string;
+}
+
+const arr: iDate[] = [
+    { date: "10.01.2017" },
+    { date: "05.11.2016" },
+    { date: "21.12.2002" },
+]
+
+function sort(arr: iDate[]): iDate[] {
+    for (let j = arr.length - 1; j > 0; j--) {
+        for (let i = 0; i < j; i++) {
+
+            let day = arr[i].date.replaceAll(".", "").split(" ")
+            let day2 = arr[i+1].date.replaceAll(".", "").split(" ")
+            if (day[0] > day2[0] ) {
+                let middle = arr[i].date
+                arr[i].date = arr[i+1].date
+                arr[i+1].date = middle
+            }
+        }
+    }
+    return arr
+}
+console.log(sort(arr));
 
